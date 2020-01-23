b6553ca1354b731f4f36268e360218db1ea8f8a7
flaviocomi <flabio.co91@gmail.com>
2020-01-23 19:21:43 +0100

    added functionalities

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,17 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 use App\Category;
 use App\Post;
+use App\Mail\MailableName;
+
 
 class HomeController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this -> middleware('auth');
     }
-    
+
     public function index()
     {
         return view('home');
--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,22 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', 'MainController@index') -> name('home.index');
 
-Route::get('/', 'MainController@index')->name('home.index');
+Route::get('/post/show/{id}', 'MainController@postShow') -> name('post.show') -> middleware('auth');
 
-Route::get('/post/show/{id}', 'MainController@postShow')->name('post.show')->middleware('auth');
+Route::get('/post/edit/{id}', 'MainController@postEdit') -> name('post.edit');
+Route::post('/post/edit/{id}', 'MainController@postUpdate') -> name('post.update');
 
-Route::get('/post/edit/{id}', 'MainController@postEdit')->name('post.edit');
-Route::post('/post/edit/{id}', 'MainController@postUpdate')->name('post.update');
+Route::get('/post/delete/{id}', 'MainController@postDelete') -> name('post.delete');
 
-Route::get('/post/delete/{id}', 'MainController@postDelete')->name('post.delete');
+Route::get('/category/edit/{id}', 'MainController@categoryEdit') -> name('category.edit') -> middleware('auth');
+Route::post('/category/update/{id}', 'MainController@categoryUpdate') -> name('category.update') -> middleware('auth');
 
-Route::get('/category/edit/{id}', 'MainController@categoryEdit')->name('category.edit');
-Route::post('/category/update/{id}', 'MainController@categoryUpdate')->name('category.update');
+Route::get('/category/delete/{id}', 'MainController@categoryDelete') -> name('category.delete') -> middleware('auth');
 
-Route::get('/category/delete/{id}', 'MainController@categoryDelete')->name('category.delete');
-
-Route::get('/category/post/{id}', 'MainController@categoryPost')->name('category.post');
-Route::post('/category/post/{id}', 'MainController@categoryPostCreate')->name('category.post.create');
+Route::get('/category/post/{id}', 'MainController@categoryPost') -> name('category.post') -> middleware('auth');
+Route::post('/category/post/{id}', 'MainController@categoryPostCreate') -> name('category.post.create') -> middleware('auth');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index') -> name('home');
